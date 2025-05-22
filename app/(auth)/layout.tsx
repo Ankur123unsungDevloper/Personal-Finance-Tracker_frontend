b8cb633cad71d02e639d34f3fa5d726b1cfe7059
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-05-22 23:40:28 +0530

    modified:   app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx 	modified:   app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx 	modified:   app/(auth)/layout.tsx 	new file:   public/logo.svg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
+
 export default function AuthLayout({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="flex items-center justify-center h-full">
-      {children}
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">
+            Welcome Back to our platform!
+          </h1>
+          <p className="text-base text-[#7ECA0]">
+            Log in to get back to your dashboard!
+          </p>
+        </div>
+        {children}
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image
+          src="/logo.svg"
+          height={100}
+          width={100}
+          alt="logo"
+        />
+      </div>
     </div>
-  )
+  );
 }
