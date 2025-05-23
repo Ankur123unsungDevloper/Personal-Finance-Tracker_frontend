86a66dfb41f596efbb675d9c6f28cb98c6eed8cb
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2025-05-24 01:35:22 +0530

    new file:   app/(dashboard)/layout.tsx 	modified:   app/(dashboard)/page.tsx 	modified:   app/api/[[...route]]/route.ts 	new file:   components/header-logo.tsx 	new file:   components/header.tsx 	new file:   components/nav-button.tsx 	new file:   components/navigation.tsx 	new file:   components/ui/button.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ components/navigation.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import { NavButton } from "@/components/nav-button";
+
+const routes = [
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/accounts",
+    label: "Accounts",
+  },
+  {
+    href: "/caterories",
+    label: "Caterories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
+
+export const Navigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          href={route.href}
+          label={route.label}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  )
+}
